b09fe11d4189b7e42a0c5f81253c348caa9145f4
hlhote <henri.lhote@tropicsquare.com>
2025-02-28 14:38:31 +0100

    Used configuration model in tests

--- tests/test_model/conftest.py
+++ tests/test_model/conftest.py
@@ -4,13 +4,14 @@ from typing import Any, Dict
 import pytest
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 
+from tvl.configuration_file_model import ConfigurationFileModel
 from tvl.constants import S_HI_PUB_NB_SLOTS
 from tvl.host.host import Host
 from tvl.targets.model.tropic01_model import Tropic01Model
 
 
 @pytest.fixture(scope="function")
-def configuration():  # type: ignore
+def configuration():
     tropic_priv_key_ = X25519PrivateKey.generate()
     tropic_priv_key_bytes = tropic_priv_key_.private_bytes_raw()
     tropic_pub_key_bytes = tropic_priv_key_.public_key().public_bytes_raw()
@@ -21,23 +22,25 @@ def configuration():  # type: ignore
         key.public_key().public_bytes_raw() for key in host_priv_keys_
     ]
 
-    yield {
-        "host": {
-            "s_h_priv": host_priv_keys_bytes,
-            "s_h_pub": host_pub_keys_bytes,
-            "s_t_pub": tropic_pub_key_bytes,
-            "pairing_key_index": (pki := random.randrange(S_HI_PUB_NB_SLOTS)),
-        },
-        "model": {
-            "s_t_priv": tropic_priv_key_bytes,
-            "s_t_pub": tropic_pub_key_bytes,
-            "i_pairing_keys": {
-                pki: {
-                    "value": host_pub_keys_bytes[pki],
+    yield ConfigurationFileModel.parse_obj(
+        {
+            "host": {
+                "s_h_priv": host_priv_keys_bytes,
+                "s_h_pub": host_pub_keys_bytes,
+                "s_t_pub": tropic_pub_key_bytes,
+                "pairing_key_index": (pki := random.randrange(S_HI_PUB_NB_SLOTS)),
+            },
+            "model": {
+                "s_t_priv": tropic_priv_key_bytes,
+                "s_t_pub": tropic_pub_key_bytes,
+                "i_pairing_keys": {
+                    pki: {
+                        "value": host_pub_keys_bytes[pki],
+                    },
                 },
             },
-        },
-    }
+        }
+    ).dict(exclude_none=True)
 
 
 @pytest.fixture(scope="function")
