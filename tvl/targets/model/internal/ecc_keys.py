2498320bb4a0351de412a3f2a1cf9beef4f1d47a
vmasek <vit.masek@tropicsquare.com>
2026-03-03 11:58:19 +0100

    feat (crypto): update Ed25519 key generation to reflect the new method

--- tvl/targets/model/internal/ecc_keys.py
+++ tvl/targets/model/internal/ecc_keys.py
@@ -199,8 +199,11 @@ class EdDSAKeyMemLayout(EccKey, curve=CurveTypes.ED25519):
 
     @classmethod
     def from_random_source(cls, rng: _RandomSource, origin: Origins) -> Self:
-        return cls.from_key(rng.urandom(EDDSA_KEY_SIZE, swap_endianness=False), origin)
+        r1 = int.from_bytes(rng.urandom(EDDSA_KEY_SIZE, swap_endianness=False))
+        r2 = int.from_bytes(rng.urandom(EDDSA_KEY_SIZE, swap_endianness=False))
+        key = int.to_bytes(((r2 << 256) | r1) % (2**256 - 1), 32)
 
+        return cls.from_key(key, origin)
 
 class EccKeys:
     def __init__(self) -> None:
